a3bcefdad3a54e107fcc06b5b539bed77b94518a
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-08-17 00:53:54 +0530

    Removing a random variable

--- demo.rb
+++ demo.rb
@@ -6,35 +6,30 @@ require 'screen'
 class Demo < Processing::App
 	app = self
 	def setup
-		@width = width
-		@height = height
-		frame_rate(30)
 		smooth
 		background(0,0,0)
 		color_mode(RGB, 1.0)
 
 		points = []
-
 		100.times {points << {:x => random(200), :y => random(300)}}
 
-		@x_unit_vector = {:x => 1.0, :y => 0.0}
-		@y_unit_vector = {:x => 0.0, :y => 1.0}
+		x_basis_vector = {:x => 1.0, :y => 0.0}
+		y_basis_vector = {:x => 0.0, :y => 1.0}
 
 		x_range = ContinuousRange.new({:minimum => 0, :maximum => 200})
 		y_range = ContinuousRange.new({:minimum => 0, :maximum => 300})
 
-		@c = CoordinateSystem.new(Axis.new(@x_unit_vector,x_range), Axis.new(@y_unit_vector,y_range), [[4,0],[0,2]], self)
-		@screen_transform = SignedTransform.new({:x => 10, :y => -1}, {:x => 300, :y => 900})
-		@screen = Screen.new(@screen_transform, self)
-		@screen.draw_axes(@c,10,10)
+		basis = CoordinateSystem.new(Axis.new(x_basis_vector,x_range), Axis.new(y_basis_vector,y_range), [[4,0],[0,2]], self)
+		screen_transform = SignedTransform.new({:x => 10, :y => -1}, {:x => 300, :y => 900})
+		screen = Screen.new(screen_transform, self)
+		screen.draw_axes(basis,10,10)
 		stroke(1,1,0,1)
 		fill(1,1,0)
 		points.each do |p|
-			@screen.plot(p, @c)
+			screen.plot(p, basis)
 		end
 	end
 	  
-
 	def draw
 	end
 end
